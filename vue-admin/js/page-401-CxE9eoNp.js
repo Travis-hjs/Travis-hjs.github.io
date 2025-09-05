69bbdeda5f6e948eb0ff3a137c3effa8d04667a8
Travis-黄景圣 <1141752341@qq.com>
2025-09-05 16:34:05 +0800

    update

--- vue-admin/js/page-401-CxE9eoNp.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-CHUGsaxc.js";import{g as o,a as s,h as t}from"./vendor-iAWo1EIP.js";const a={name:"page-401"},c={class:"page-401"},n=t("div",null,"暂无权限访问",-1),r=[n];function _(p,u,d,i,f,l){return s(),o("div",c,r)}const B=e(a,[["render",_]]);export{B as default};
